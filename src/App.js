89e9f0c509fa2dd9b5aab19e6ee2ca23faa538be
Soumajit2000 <soumajitchoudhuri@gmail.com>
2021-07-09 02:18:51 +0530

    adding code files and making 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddTask from "./components/AddTask";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [showAddtask, setShowAddtask] = useState(false)
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Doctors Appoitment",
+			day: "Feb 5th at 2:30pm",
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: "Meeting at School",
+			day: "Feb 6th at 1:30pm",
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: "Food Shoppingt",
+			day: "Feb 7th at 2:30pm",
+			reminder: false,
+		},
+	]);
+
+	//Add Tasks
+	const addTask = (task) => {
+		 const id = Math.floor(Math.random()*10000) + 1
+		 const newTask = {id, ...task}
+		 setTasks([...tasks, newTask])
+	};
+
+	//Delete tasks
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
+
+	//Toggle reminder
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		);
+	};
+	return (
+		<div className="container">
+			<Header onAdd={()=> setShowAddtask(!showAddtask)} showAdd = {showAddtask}/>
+			{showAddtask && <AddTask onAdd={addTask} />}
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+			) : (
+				<h4>No tasks Added</h4>
+			)}
+		</div>
+	);
+};
 
 export default App;
